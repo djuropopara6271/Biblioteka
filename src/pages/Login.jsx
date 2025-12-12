9071f6193819794c6c271c720c9b9b94f1c55ed1
djuropopara6271 <djuro.popara.6271@metropolitan.ac.rs>
2025-12-12 18:11:07 +0100

    Add login functionality with json-server

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,64 @@
+import { useState } from "react";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.get(
+        `http://localhost:3001/users?email=${email}&password=${password}`
+      );
+
+      if (res.data.length === 0) {
+        setError("Pogrešan email ili lozinka");
+        return;
+      }
+
+      login(res.data[0]);
+      navigate("/");
+    } catch {
+      setError("Greška pri povezivanju sa serverom");
+    }
+  };
+
   return (
     <div>
       <h1>Prijava</h1>
-      <p>(forma dolazi u sledećem commitu)</p>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Lozinka"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 }
